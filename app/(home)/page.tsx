46830280a2b4f11407d616a7c5b929f0e5155b3e
Charlie Meyer <charlie.g.meyer@icloud.com>
2024-06-20 14:43:07 -0500

    text

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">
-        <span className="text-black">Simple</span>
+        <span className="text-black dark:text-white">Simple</span>
         <span className="text-theme-primary">Text</span>
       </h1>
     </div>
