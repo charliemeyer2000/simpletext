ab87566cc5bf0225f54f4a25efcb69f089341e1f
Charlie Meyer <charlie.g.meyer@icloud.com>
2024-06-24 15:23:57 -0500

    landing page simplification

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {ThemeToggle} from "@/components/custom/themeToggle";
 import {Button} from "@/components/ui/button";
+import {Skeleton} from "@/components/ui/skeleton";
 import {CommandSearchBar} from "@/components/custom/commandSearchBar";
 import ArrowUpRight from "@/public/svg/arrow-up-right.svg";
 import Image from "next/image";
@@ -14,7 +15,10 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+} from "@/components/ui/navigation-menu";
+// import react
+import React from "react";
+import {cn} from "@/lib/utils";
 
 export default function Home() {
 
@@ -32,16 +36,15 @@ export default function Home() {
               </Link>
             </div>
             <div className="flex flex-grow justify-center items-center gap-x-4 opacity-80">
-              <NavigationMenu>
-                <NavigationMenuList>
-                  <NavigationMenuItem>
-                    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                      <NavigationMenuLink>Link</NavigationMenuLink>
-                    </NavigationMenuContent>
-                  </NavigationMenuItem>
-                </NavigationMenuList>
-              </NavigationMenu>
+              <Link href="mailto:contact@simpletext.dev" className="text-sm text-grey hover:text-primary">
+                Contact
+              </Link>
+              <Link href="#pricing" scroll={false} className="text-sm text-grey hover:text-primary">
+                Pricing
+              </Link>
+              <Link href="https://docs.simpletext.dev" target="_blank" rel="noopener noreferrer" className="relative text-sm text-grey hover:text-primary">
+                <span>Docs</span>
+              </Link>
             </div>
             <div className="flex flex-row ml-auto items-center gap-x-4">
               <CommandSearchBar />
@@ -79,3 +82,49 @@ export default function Home() {
     </>
   );
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link> & {title: string}
+>(({className, title, children, ...props}, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Link
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="font-medium text-sm leading-none">{title}</div>
+          <p className="line-clamp-2 text-muted-foreground text-sm leading-snug">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
+
+const BrandListItem = ({className}: {className?: string}) => {
+  return (
+    <li className={className}>
+      <NavigationMenuLink asChild>
+        <Link
+          className="flex flex-col justify-start bg-gradient-to-b from-muted/50 to-muted focus:shadow-md p-6 rounded-md w-full h-full no-underline select-none outline-none"
+          href="/"
+        >
+          <img src="/favicon.png" alt="Logo" className="rounded-xl w-20 h-20" />
+          <div className="mt-4 mb-2 font-medium text-lg">Audoly</div>
+          <p className="text-muted-foreground text-sm leading-tight">
+            Playlisting made easy. Find playlist curators, get contact
+            information, and take your music to the next level.
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+};
